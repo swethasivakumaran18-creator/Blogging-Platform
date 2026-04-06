e4301d9b736863ae712d262b40454684ceb62597
Swetha <swethasivakumaran18@gmail.com>
2026-04-06 22:31:18 +0530

    Create Notification.java

---
+++ Notification.java
@@ -0,0 +1,5 @@
+public class Notification {
+    public static void notify(String owner, String message) {
+        System.out.println("🔔 Notification to " + owner + ": " + message+"\n\n\n");
+    }
+}
